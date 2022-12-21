5adcf824ebb99c05bcdca10c9ded13bdbcd29be0
sembori <sembori@yandex.ru>
2022-12-21 23:59:35 +0300

    Weekend or not

---
+++ HW2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+Write ("Введите номер дня недели: ");
+int day = Convert.ToInt32(ReadLine());
+if (day > 7){
+WriteLine("К сожалению, в неделе меньше дней.");
+}else if(day < 1){
+    WriteLine("Нулевого дня в неделе нет. Хоть и хотелось бы еще денечек перед понедельником.");
+}else{
+WriteLine (day > 5? "Да" : "Нет" );
+}
