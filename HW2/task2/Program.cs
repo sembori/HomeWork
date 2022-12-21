f77356cbc3df97dde5a7c05e9898906cb8f5875c
sembori <sembori@yandex.ru>
2022-12-21 23:48:08 +0300

    Print third number

---
+++ HW2/task2/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите любое число: ");
+int number = Convert.ToInt32(ReadLine());
+
+WriteLine (number < 100? "Третьей цифры нет(" : $"Третья цифра: {number/100%10}");
