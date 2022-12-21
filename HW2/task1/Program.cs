b986c6752ed55363718702ba6494e86defb715a6
sembori <sembori@yandex.ru>
2022-12-21 23:37:47 +0300

    Print second numbers

---
+++ HW2/task1/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine(a < 100 || a > 999? "Число должно быть трехзначным" : $"Второе число:  {a/10%10} ");
+
