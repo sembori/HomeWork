2972ecd71b0f75da89b5a17b0b1bc13d8bb4105e
sembori <sembori@yandex.ru>
2022-12-27 21:19:46 +0300

    Distance_in_3D

---
+++ HW3/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Для того, чтобы узнать расстояние между точками в пространстве введите их координаты.");
+Console.Write("Введите координату x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между точками = {distance:f2}");
