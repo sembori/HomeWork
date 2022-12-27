46ca23d6e353f9323965b8f76f0002efd1c217f3
sembori <sembori@yandex.ru>
2022-12-27 21:08:57 +0300

    Polyndroms_or_not

--- HW3/task1/Program.cs
+++ HW3/task1/Program.cs
@@ -4,4 +4,13 @@ Clear();
 Write("Введите пятизначное число: ");
 int a = Convert.ToInt32(ReadLine());
 
-WriteLine((a/1000 + a%100)%11 == 0 ? "ДА" : "НЕТ");
+if (a > 99999)
+{
+    WriteLine("Введите меньшее число");
+}
+else if (a < 10000){
+    WriteLine("Введите большее число");
+}else
+{
+    WriteLine((a / 1000 + a % 100) % 11 == 0 ? "ДА" : "НЕТ");
+}
