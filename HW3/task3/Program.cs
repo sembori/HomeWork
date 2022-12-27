ede5bbb0de753af646a426c652508cfddeb4e946
sembori <sembori@yandex.ru>
2022-12-27 21:54:47 +0300

    Coubes_from1_toN

---
+++ HW3/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+Clear();
+int i = 1;
+Write("Сейчас мы выведем все кубы чисел от 1 до: ");
+int.TryParse(ReadLine(), out int n);
+
+if (n <= 1)
+{
+    WriteLine("Число должно быть больше 1!");
+
+}
+else
+{
+    while (i <= n)
+    {
+        WriteLine($"Куб числа {i} - {Math.Pow(i, 3)} ");
+        i++;
+    }
+}
