d5c2028248df1e2e428353f3f10ec8941bcc99f2
sembori <sembori@yandex.ru>
2023-01-17 21:34:42 +0300

    A_in_grade_B_programm_writed

---
+++ HW4/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 25: Напишите программу, реализующую метод,который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
+using static System.Console;
+Clear();
+WriteLine("Возведем число A в степень B.");
+Write("Введите число A: ");
+int.TryParse(ReadLine(), out int A);
+Write("Введите число B: ");
+int.TryParse(ReadLine(), out int B);
+
+if (B > 0)
+{
+    WriteLine($"{A} в степени {B} = {AInGradeB(A, B)}");
+}
+else
+{
+    WriteLine("Степень должна быть больше 0");
+}
+
+int AInGradeB(int a, int b)
+{
+    int result = a;
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+        i++;
+    }
+    return result;
+}
+
