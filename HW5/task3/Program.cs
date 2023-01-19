7b8a92350a28ab22dc1c63d8c55bdae10ae8b401
sembori <sembori@yandex.ru>
2023-01-19 19:54:25 +0300

    Difference_maximum_and_minimum

---
+++ HW5/task3/Program.cs
@@ -0,0 +1,46 @@
+﻿//  Напишите программу реализующую методы формирования массива, заполненного случайными вещественными числами и вычисления разницы между максимальным и минимальным элементов массива.
+using static System.Console;
+Clear();
+Write("Введите через пробел длинну массива, его минимальное значение и максимальное значение: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+double[] numbers = GetArray(int.Parse(parameters[0]), double.Parse(parameters[1]), double.Parse(parameters[2]));
+Write("Ваш массив: ");
+PrintArray(numbers);
+double minimum = double.Parse(parameters[2]);
+double maximum = 0;
+foreach (var element in numbers)
+{
+    if (element < minimum)
+    {
+        minimum = element;
+    }
+    if (element > maximum)
+    {
+        maximum = element;
+    }
+}
+Write($"Разница максимального и минимального элемента = {maximum-minimum}");
+
+double[] GetArray(int size, double minValue, double maxValue)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+    }
+    return array;
+}
+
+void PrintArray(double[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]} | ");
+    }
+    WriteLine($"{inArray[inArray.Length - 1]}]");
+}
+
+
+
+
