07cd56c787e2fc72b4fd3b38aacc58bff644bcad
sembori <sembori@yandex.ru>
2023-02-05 00:29:00 +0300

    N_to_1_recursion

---
+++ HW9/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая реализует рекурсивный метод вывода всех натуральных числел в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int num = int.Parse(ReadLine());
+PrintNumbers(num);
+
+void PrintNumbers(int number)
+{
+    if (number == 1)
+    {
+        Write($"{1}. ");;
+    }
+    else
+    {
+        Write($"{number}, ");
+        PrintNumbers(number - 1);
+    }
+}
