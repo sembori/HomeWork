2c4eae94cb91c51e21a0d11eddc6d0379e492219
sembori <sembori@yandex.ru>
2023-02-05 00:59:29 +0300

    AkkermanFunction

---
+++ HW9/task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу реализующую метод вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+using static System.Console;
+Clear();
+Write("Введите два натуральных числа M и N через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Console.Write($"Функция Аккермана = {FunctionAkkerman(int.Parse(parameters[0]), int.Parse(parameters[1]))} ");
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return FunctionAkkerman(m - 1, 1);
+    }
+    else
+    {
+        return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
+    }
+}
+
+
