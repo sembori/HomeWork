83619f9e787a39a5f0e9880d17cbce849ebed5fc
sembori <sembori@yandex.ru>
2023-02-05 00:52:37 +0300

    numbersSum

---
+++ HW9/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая реализует рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+using static System.Console;
+Clear();
+Write("Введите два натуральных числа M и N через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+NumberSum(int.Parse(parameters[0]), int.Parse(parameters[1]), 0);
+
+
+void NumberSum(int numberM, int numberN, int sum)
+{
+    if (numberM > numberN)
+    {
+        Write($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + numberM++;
+    NumberSum(numberM, numberN, sum);
+}
